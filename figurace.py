ccb1145fb7275abc7a9302ff82126a97c12d888c
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2022-05-23 10:24:54 -0300

    Cambios en la interfaz

--- figurace.py
+++ figurace.py
@@ -1,15 +1,7 @@
-from src.core import menu_config
-from src.core import menu_perfiles
 from src.core import menu_principal
+
 if __name__ == "__main__":
     print("Figurace")
-
-
-# Para probar el menu de perfiles
-# menu_perfiles.iniciar_menu_perfiles()
-
-# Para probar el menu config
-# menu_config.iniciar_pantalla_config()
 
 # Para probar el menu principal
 menu_principal.iniciar_menu_principal()
--- src/core/menu_principal.py
+++ src/core/menu_principal.py
@@ -29,15 +29,13 @@ def crear_ventana_nuevo_jugador():
 
 def crear_ventana_principal():
 
-    menu = [
-        [sg.Text("FIGURACE", justification="center",
-                font="Helvetica 15 bold", pad=((0, 0), (0, 10)))],
+    elem_botones = [sg.Col(layout=[
         [sg.Button("Jugar", key="-JUGAR-", font="Helvetica 13")],
         [sg.Button("Configuracion", key="-CONFIG-", font="Helvetica 13")],
         [sg.Button("Puntajes", key="-PUNTAJES-", font="Helvetica 13")],
         [sg.Button("Perfil", key="-PERFIL-", font="Helvetica 13")],
-        [sg.Button("Salir", key="-EXIT-", font="Helvetica 13")],
-        ]
+        [sg.Button("Salir", key="-EXIT-", font="Helvetica 13")]])]
+        
 
     #Obtiene la ultima sesion abierta
     ultima_sesion = manejar_datos.obtener_ultima_sesion()
@@ -50,20 +48,21 @@ def crear_ventana_principal():
     for jugador in jugadores:
         perfiles.append(jugador["nick"])
 
-    layout_configs = [
-        [sg.Frame(title="Usuario", element_justification="center", title_location="n", pad=((50, 0), (0, 150)), border_width=0,
-                    layout=[[sg.Combo(perfiles, key="-USER-", font="Helvetica 11", size=(20, 1), enable_events=True, readonly=True, default_value=ultima_sesion["nick"])]])],
+    elem_usuario = [sg.Frame(title="Usuario", element_justification="center", title_location="n", border_width=0, layout=[[sg.Combo(perfiles, key="-USER-", font="Helvetica 11", size=(20, 1), enable_events=True, readonly=True, default_value=ultima_sesion["nick"])]])]
 
-        [sg.Frame(title="Dificultad", element_justification="center", title_location="n", pad=((50, 0), (0, 150)), border_width=0,
-                    layout=[[sg.Combo(["Facil", "Normal", "Dificil", "Einstein"], size=(20, 1), font="Helvetica 11", key="-MODE-", enable_events=True, readonly=True, default_value=ultima_sesion["dificultad"])]])]
-    ]
+    elem_dificultad = [sg.Frame(title="Dificultad", element_justification="center", title_location="n", border_width=0, layout=[[sg.Combo(["Facil", "Normal", "Dificil", "Einstein"], size=(20, 1), font="Helvetica 11", key="-MODE-", enable_events=True, readonly=True, default_value=ultima_sesion["dificultad"])]])]
+    
+    elem_sesion = [sg.Frame(title="Última partida", title_location="n", border_width=1,
+                    layout=[[sg.Text(ultima_sesion["nick"], font="Helvetica 11")]])]
 
-    layout = [
-        [sg.Col(menu, element_justification='center',  pad=(
-            (190, 0), (100, 100))), sg.Col(layout_configs)]
-    ]
+    elem_configs = [sg.Col(layout=[elem_usuario, elem_dificultad,elem_sesion])]
 
-    return sg.Window("Figurace", layout, size=(500, 500), finalize=True)
+    elem_titulo = [sg.Text("FIGURACE", justification="center", font="Helvetica 15 bold", pad=((0, 0), (0, 10)))]
+
+    elem_frame = [sg.Frame(title="Menú", element_justification="center", title_location="n", expand_x=True, border_width=1,
+                layout=[elem_botones,elem_configs])]
+
+    return sg.Window("Figurace", layout=[[sg.Col(layout=[elem_titulo,elem_frame], element_justification='center')]], size=(500, 500), finalize=True)
 
 
 def iniciar_menu_principal():
@@ -73,6 +72,7 @@ def iniciar_menu_principal():
         event, values = window.read()
 
         if(event == sg.WIN_CLOSED) or (event == "-EXIT-"):
+
             break
         elif event == "-JUGAR-":
             window.close()
