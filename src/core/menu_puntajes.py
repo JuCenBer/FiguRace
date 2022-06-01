a7c548b6c59fdcbbb2a95fc505d999aa1cbcf818
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2022-05-31 21:49:30 -0300

    Mejorada la pantalla de puntajes

--- src/core/menu_puntajes.py
+++ src/core/menu_puntajes.py
@@ -4,20 +4,23 @@ import PySimpleGUI as sg
 from . import manejar_datos
 
 def iniciar_pantalla_puntajes():
-    '''Crea la ventana de la pantalla de puntajes para visualizar los mejores 20 puntajes por cada dificultad '''
-    # Seguramente se tengan que hacer distintas funciones para las distintas dificultades
+    ''' Crea la ventana de la pantalla de puntajes para visualizar los mejores 20 puntajes por cada dificultad '''
+    # Se obtendran los historiales de partidas con el modulo manejar_datos
     #tabla_top_usuarios = manejar_datos.obtener_top_puntajes()
 
+    # Se hace una variable a modo de ejemplo para visualizar en la primera entrega
     tabla_top_usuarios = [["Usuario 1","112154"],["Usuario 2","45468"]]
 
     # Varias tablas para las distintas dificultades
-    elem_tabla = sg.Table(headings=["Usuario","Puntos"], values=tabla_top_usuarios, auto_size_columns=True, font="Helvetica 13", hide_vertical_scroll=True, num_rows=20, justification="center")
+    elem_tabla_1 = sg.Frame(title='Fácil', title_location='n', layout=[[sg.Table(headings=["Usuario","Puntos"], values=tabla_top_usuarios, auto_size_columns=True, font="Helvetica 13", hide_vertical_scroll=True, num_rows=20, justification="center")]])
 
-    elem_tabla_2 = sg.Table(headings=["Usuario","Puntos"], values=tabla_top_usuarios, auto_size_columns=True, font="Helvetica 13", hide_vertical_scroll=True, num_rows=20, justification="center")
+    elem_tabla_2 = sg.Frame(title='Normal', title_location='n', layout=[[sg.Table(headings=["Usuario","Puntos"], values=tabla_top_usuarios, auto_size_columns=True, font="Helvetica 13", hide_vertical_scroll=True, num_rows=20, justification="center")]])
 
-    elem_tabla_3 = sg.Table(headings=["Usuario","Puntos"], values=tabla_top_usuarios, auto_size_columns=True, font="Helvetica 13", hide_vertical_scroll=True, num_rows=20, justification="center")
+    elem_tabla_3 = sg.Frame(title='Difícil', title_location='n', layout=[[sg.Table(headings=["Usuario","Puntos"], values=tabla_top_usuarios, auto_size_columns=True, font="Helvetica 13", hide_vertical_scroll=True, num_rows=20, justification="center")]])
 
-    elem_tablas = [elem_tabla, elem_tabla_2, elem_tabla_3]
+    elem_tabla_4 = sg.Frame(title='Einstein', title_location='n', layout=[[sg.Table(headings=["Usuario","Puntos"], values=tabla_top_usuarios, auto_size_columns=True, font="Helvetica 13", hide_vertical_scroll=True, num_rows=20, justification="center")]])
+
+    elem_tablas = [elem_tabla_1, elem_tabla_2, elem_tabla_3, elem_tabla_4]
     
     elem_volver = [sg.Button("Volver",key="-PUNTAJES_VOLVER-", font="Helvetica 13",s=(10, 1))]
 
