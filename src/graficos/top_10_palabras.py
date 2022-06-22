63a1d842dd91bd6573d114f13124429b7d05b9ae
Chuliannn <julianbertonb@gmail.com>
2022-06-22 20:55:06 -0300

    Cambios

--- src/graficos/top_10_palabras.py
+++ src/graficos/top_10_palabras.py
@@ -10,15 +10,4 @@ filas_respuestas_correctas = data_set[(data_set["texto_ingresado"] == data_set["
 
 diez_palabras = filas_respuestas_correctas["respuesta"].sort_values(ascending=False).head(10)
 
-#Aca va el grafico barras
-etiquetas = ["Hombres", "Mujeres", "No Binario"]
-
-data_dibujo = [cant_hombres, cant_mujeres, cant_nobinario]
-explode = (0.1, 0.1, 0.1)
-
-plt.pie(data_dibujo, explode=explode, labels=etiquetas, autopct='%1.2f%%', shadow=True, startangle=120, labeldistance= 1.2)
-plt.axis('equal') 
-plt.legend(etiquetas)
-
-plt.title("Porcentaje de partidas finalizadas por genero")
-plt.show()
+print(diez_palabras)
